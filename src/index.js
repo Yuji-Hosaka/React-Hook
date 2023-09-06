9b21d5ba182b69d254af8b2f8abf013f95fddf25
Yuji-Hosaka <h.kyouiku58@gmail.com>
2023-09-06 18:16:11 +0700

    Setup useEffect

--- src/index.js
+++ src/index.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+// step-1 : รู้ว่า user กดปุ่มไหน?
+
+function App () {
+  const [category, setCategory] = useState('');
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log('Effect Hook');
+  });
+  return(
+    <div>
+      <h1>useEffect : {category || 'not-select'}</h1>
+      <div className='button__group'>
+        <button onClick={() => setCategory('posts')}>posts</button>
+        <button onClick={() => setCategory('photos')}>photos</button>
+        <button onClick={() => setCategory('to-do')}>to-do</button>
+        <button onClick={() => setCategory('users')}>users</button>
+        <button onClick={() => setCount((c) => c + 1)}>+</button>
+        <div>{count}</div>
+        <button onClick={() => setCount((c) => c - 1)}>-</button>
+      </div>
+
+      <ul className='lists'>
+        <li className='lists__item'>item-1</li>
+      </ul>
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>React Hook</h1>);
+root.render(<App/>);
