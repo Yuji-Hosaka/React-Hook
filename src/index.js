1832cfe951fadf078e1e3a800d5cd746d1ae9cbd
Yuji-Hosaka <h.kyouiku58@gmail.com>
2023-09-06 16:23:50 +0700

    Finish simple search

--- src/index.js
+++ src/index.js
@@ -1,7 +1,70 @@
-import React from 'react';
+import React,{useState} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+//step-1 : เก็บ Input จาก User => React State
+//step-2 : handle จังหวะตอน submit
+//step-3 : Make HTTP Request => Get Data
+//step-4 : เอา data มาแสดงผล (เก็บเป็น React state)
+
+const BASE_URL = 'https://api.themoviedb.org/3';
+const TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MDRhZDg0YTEzODRkMDRjNGY0YWVlMzZmNGE5YjE0OCIsInN1YiI6IjVlZWUwOTAwYjA0NjA1MDAzNDBlOTg2MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.H7gQK0dgqOvsQmsHsgtOmsqKVFmnb1urXF3q9AkqyCE';
+
+function App() {
+  const [keyword, setKeyword] = useState("");
+  const [movieLists, setMovieLists] = useState([])
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    let url = `${BASE_URL}/search/keyword?query=${keyword}&page=1`;
+    let option = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${TOKEN}`,
+      },
+    };
+    try {
+      const response = await fetch(url,option);
+      const data = await response.json();
+      setMovieLists(data.results);
+    } catch (error) {
+      console.log(error)
+    }
+  };
+  return (
+    <div className='App'>
+      {/* Header */}
+      <div className='header'>
+        <h1>Movie 検索</h1>
+      </div>
+
+      {/* Input */}
+      <form className='search' onSubmit={handleSubmit} >
+        <input 
+        type='text' 
+        placeholder='keyword ?' 
+        value={keyword}
+        onChange= {(e) => setKeyword(e.target.value)}
+        />
+        <button type='submit'>search</button>
+
+      </form>
+
+      {/* Result */}
+      <div className='movie-lists'>
+        {movieLists.map((movie) => (
+          <div 
+          key={movie.id}
+          className='movie'>
+          {movie.name}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>React Hook</h1>);
+root.render(<App />);
